1e5994bbf2805019c07f6ba2c57305748c898964
Zhongyi Fu <zyfu90@gmail.com>
2024-04-06 11:57:29 -0600

    add try catch

--- index.mjs
+++ index.mjs
@@ -4,6 +4,7 @@ import cors from 'cors';
 
 const port = process.env.PORT || 80;
 const app = express();
+const islocal = process.env.LOCAL || false;
 
 app.use(cors());
 app.options('*', cors());
@@ -11,47 +12,68 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/patients', (req, res) => {
-  Patients.scan()
-    .loadAll()
-    .exec(function (err, patients) {
-      if (err) {
-        return res.status(400).json({ error: err.message });
-      }
-      return res.json(patients);
-    });
+  try {
+    Patients.scan()
+      .loadAll()
+      .exec(function (err, patients) {
+        if (err) {
+          return res.status(500).json({ error: err.message });
+        }
+        return res.json(patients);
+      });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 });
 
 app.get('/patients/:id', (req, res) => {
-  Patients.query(req.params.id).exec(function (err, patient) {
-    if (err) {
-      return res.status(400).json({ error: err.message });
-    }
-    return res.json(patient);
-  });
+  try {
+    Patients.query(req.params.id).exec(function (err, patient) {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      return res.json(patient);
+    });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 });
 
 app.post('/patients', async (req, res) => {
-  const newPatient = new Patients(req.body);
-  await newPatient.save();
-  return res.status(201).json({ id: newPatient.id });
+  try {
+    const newPatient = new Patients(req.body);
+    console.log(newPatient);
+    await newPatient.save();
+    return res.status(201).json({ id: newPatient.id });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 });
 
 app.put('/patients/:id', (req, res) => {
-  Patients.update({ id: req.params.id, ...req.body }, function (err, data) {
-    if (err) {
-      return res.status(400).json({ error: err.message });
-    }
-    return res.sendStatus(200);
-  });
+  try {
+    Patients.update({ id: req.params.id, ...req.body }, function (err, data) {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      return res.sendStatus(200);
+    });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 });
 
 app.delete('/patients/:id', (req, res) => {
-  Patients.destroy({ id: req.params.id }, function (err, data) {
-    if (err) {
-      return res.status(400).json({ error: err.message });
-    }
-    return res.sendStatus(204);
-  });
+  try {
+    Patients.destroy({ id: req.params.id }, function (err, data) {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      return res.sendStatus(204);
+    });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 });
 
 app.get('/records', (req, res) => {
@@ -59,7 +81,7 @@ app.get('/records', (req, res) => {
     .loadAll()
     .exec(function (err, records) {
       if (err) {
-        return res.status(400).json({ error: err.message });
+        return res.status(500).json({ error: err.message });
       }
       return res.json(records);
     });
@@ -68,7 +90,7 @@ app.get('/records', (req, res) => {
 app.get('/records/:id', (req, res) => {
   Records.query(req.params.id).exec(function (err, data) {
     if (err) {
-      return res.status(400).json({ error: err.message });
+      return res.status(500).json({ error: err.message });
     }
     return res.json(data);
   });
@@ -88,7 +110,7 @@ app.post('/records', async (req, res) => {
 app.put('/records/:id', (req, res) => {
   Records.update({ id: req.params.id, ...req.body }, function (err, data) {
     if (err) {
-      return res.status(400).json({ error: err.message });
+      return res.status(500).json({ error: err.message });
     }
     return res.status(200);
   });
@@ -97,7 +119,7 @@ app.put('/records/:id', (req, res) => {
 app.delete('/records/:id', (req, res) => {
   Records.destroy({ id: req.params.id }, function (err, data) {
     if (err) {
-      return res.status(400).json({ error: err.message });
+      return res.status(500).json({ error: err.message });
     }
     return res.status(204);
   });
